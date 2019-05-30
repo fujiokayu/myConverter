c9a0a248f53cf9f0c67f8267c02a24607892636b
fujiokayu <fujiokayu@gmail.com>
2019-05-30 23:18:52 +0900

    Add generic enc/dec function.

--- converter/converter.go
+++ converter/converter.go
@@ -2,6 +2,8 @@ package converter
 
 import (
 	"fmt"
+	"image"
+	"image/gif"
 	"image/jpeg"
 	"image/png"
 	"log"
@@ -10,28 +12,66 @@ import (
 	"strings"
 )
 
-//J2P : convert Jpeg file to PNG
-func J2P(filePath string) {
+type imageFile struct {
+	filePath     string
+	fileBaseName string
+	fileExt      string
+}
 
-	fmt.Println("Converting", filePath)
+func newImageFile(filePath string) *imageFile {
+	newFile := &imageFile{}
 
+	return newFile
+}
+
+func dec(filePath string, decodeType string) (image.Image, error) {
 	reader, err := os.Open(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer reader.Close()
-	m, err := jpeg.Decode(reader)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	f, err := os.Create(strings.TrimSuffix(filePath, path.Ext(filePath)) + ".png")
+	switch decodeType {
+	case ".gif":
+		return gif.Decode(reader)
+	case ".jpeg", ".jpg":
+		return jpeg.Decode(reader)
+	case ".png":
+		return png.Decode(reader)
+	}
+	return nil, nil
+}
+
+func enc(filePath string, encodeType string, m image.Image) error {
+	f, err := os.Create(strings.TrimSuffix(filePath, path.Ext(filePath)) + "." + encodeType)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	if err := png.Encode(f, m); err != nil {
+	switch encodeType {
+	case ".gif":
+		return gif.Encode(f, m, nil)
+	case ".jpeg", ".jpg":
+		return jpeg.Encode(f, m, nil)
+	case ".png":
+		return png.Encode(f, m)
+	}
+	return nil
+}
+
+//Convert : convert Jpeg file to PNG
+func Convert(filePath string, decodeType string, encodeType string) {
+
+	fmt.Println("Converting", filePath)
+
+	m, err := dec(filePath, decodeType)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = enc(filePath, encodeType, m)
+	if err != nil {
 		log.Fatal(err)
 	}
 
